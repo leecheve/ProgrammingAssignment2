0282d2e115b1657cb8fd1d5c41a6674e9f0ec81a
Luis Echeverria Arevalo <luisneto@gmail.com>
2014-09-28 15:09:56 -0500

    Solución al ejercicio de coursera de Programming Assignment 2 para crear la inversa de una matriz cacheable

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(m) {
+    x <<- m
+    i <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +22,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
+
+mymat <- makeCacheMatrix(matrix(c(1, 8, -9, 7, 5,
+                                  0, 1, 0, 4, 4,
+                                  0, 0, 1, 2, 5,
+                                  0, 0, 0, 1, -5,
+                                  0, 0, 0, 0, 1), nrow=5, ncol=5, byrow=T))
+
+mymat$get()
+cacheSolve(mymat)
